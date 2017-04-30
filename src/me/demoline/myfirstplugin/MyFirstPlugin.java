5ec96ce90ee97a71bcb0b7bc2d0b6ab8b853c3fb
DemoLine <ilya.dvorckin@ya.ru>
2017-04-30 13:59:47 +1100

    Test

--- src/me/demoline/myfirstplugin/MyFirstPlugin.java
+++ src/me/demoline/myfirstplugin/MyFirstPlugin.java
@@ -1,9 +1,6 @@
 package me.demoline.myfirstplugin;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -13,7 +10,6 @@ public class MyFirstPlugin extends JavaPlugin {
     @Override
     public  void onEnable() {
     try {
-
         Player player = event.getPlayer();
         if (player == null) return;
     } catch (Exception ex) {
