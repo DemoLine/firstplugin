91094af5a460b90ea8ed0448d537fd7b9a88d350
DemoLine <ilya.dvorckin@ya.ru>
2017-04-30 16:57:24 +1100

    Removed event

--- src/me/demoline/myfirstplugin/MyFirstPlugin.java
+++ src/me/demoline/myfirstplugin/MyFirstPlugin.java
@@ -3,6 +3,7 @@ package me.demoline.myfirstplugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.block.*;
 
 /**
  * Created by ПК on 29.04.2017.
@@ -11,13 +12,9 @@ public class MyFirstPlugin extends JavaPlugin {
     @Override
     public  void onEnable() {
     try {
-        Player player = event.getPlayer();
-        if (player == null) return;
+
     } catch (Exception ex) {
         ex.printStackTrace();
     }
     }
-}
-public class MainListener implements Listener {
-
 }
