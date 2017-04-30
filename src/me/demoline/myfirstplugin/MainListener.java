df8a3b83f61f3245d0fbda51cae91ad5768718f1
DemoLine <ilya.dvorckin@ya.ru>
2017-04-30 17:14:41 +1100

    Upd

--- src/me/demoline/myfirstplugin/MainListener.java
+++ src/me/demoline/myfirstplugin/MainListener.java
@@ -6,4 +6,10 @@ import org.bukkit.event.Listener;
  * Created by ПК on 30.04.2017.
  */
 public class MainListener implements Listener {
+    private MyFirstPlugin main;
+
+    public MainListener(MyFirstPlugin main) {
+        this.main = main;
+        main.getServer().getPluginManager().registerEvents( listener: this, main);
+    }
 }
