be0a4b442803cddb7da9a1bbc44320f12c7d4dd0
DemoLine <ilya.dvorckin@ya.ru>
2017-04-30 13:56:47 +1100

    Test

--- src/me/demoline/myfirstplugin/MainListener.java
+++ src/me/demoline/myfirstplugin/MainListener.java
@@ -4,6 +4,6 @@ import org.bukkit.event.Listener;
 /**
  * Created by ПК on 30.04.2017.
  */
-public class MainListener implements Listener{
+public class MainListener implements Listener {
     }
 }
